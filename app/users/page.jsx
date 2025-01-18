70f86a33678691a858a1ddccb5cfa5bc159b61d2
Yengzzkie <gatchalian.manuel@gmail.com>
2025-01-18 11:27:16 -0500

    add user role in user schema, conditionally render Users page if user is admin

--- app/users/page.jsx
+++ app/users/page.jsx
@@ -5,8 +5,11 @@ import { redirect } from "next/navigation";
 const UsersTablePage = async () => {
   const session = await auth();
   
+  console.log(session)
   if(!session) {
     redirect("/login")
+  } else if(session.user.role !== "ADMIN") { // Redirect to home if user is not admin
+    redirect("/")
   }
 
   return (
